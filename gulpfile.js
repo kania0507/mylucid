4c57a65d9be6113f077e98dca79c53710daa2da0
Ania <noemail@noserver.com>
2019-04-05 21:34:33 +0200

    Modifing gulpfile and index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
-var gulp = require('gulp');
-const browserSync     = require('browser-sync').create();
-/*
-gulp.task('greet', function () {
-   console.log('Hello world!');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch("src/scss/*.scss", ['sass']);
+	gulp.watch("src/scss/**/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
-*/
 
-var gulp = require('gulp'),
-   uglify = require('gulp-uglify');
-
-gulp.task('minify', function () {
-   gulp.src('js/app.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('build'))
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/scss/*.scss")
+        .pipe(sass())
+//		.on('error', handleError)
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('server', ()=>{
-  browserSync.init({
-    server: {
-        baseDir: "src"
-    },
-    browser: ['chrome']
-  });
-}); 
+gulp.task('run',  ['serve'], function(){
+	
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+	gulp.watch('application/*.html', browserSync.reload); 
+  gulp.watch('application/js/**/*.js', browserSync.reload);
+});
